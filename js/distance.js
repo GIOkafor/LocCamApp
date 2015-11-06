fb1e0c265a07cbe20f3fd3f52191913f2885cdf2
unknown <iokafor@FOR-WRKSTN-03.Formative.local>
2015-11-06 17:47:32 -0500

    working on fixing pin now

--- js/distance.js
+++ js/distance.js
@@ -1,39 +1,87 @@
-// Converts from degrees to radians.
-Math.toRadians = function(degrees) {
-  return degrees * Math.PI / 180;
-};
- 
-// Converts from radians to degrees.
-Math.degrees = function(radians) {
-  return radians * 180 / Math.PI;
-};
+var myRealtorApp = new angular.module('myRealtorApp', []);
+var map = null;
+var currInfobox = null;
 
-var calcDistance = function () {
-	console.log("Calculating distance");
-	var lat1 = -79.3391;
-	var lat2 = document.getElementById('deslat').value;
-	var lon1 = 43.7234;
-	var lon2 = document.getElementById('deslong').value;
-	var res = document.getElementById('distance');
-	console.log("aacquired dom elements");
+myRealtorApp.controller('bodyCtrl', [
+	'$scope', 
+	function($scope) {
+		$scope.listings = [
+			{'name': 'Codefusion',
+			'address': '29 Gervais Dr, Toronto ON M3C 1Y9'},
+			{'name': 'Canada Christian College',
+			'address': '50 Gervais Dr, North York ON M3C 1Z3'},
+			{'name': 'Don Mills Civitan Area',
+			'address': '1030 Don Mills Rd, North York ON M3C 1W6'},
+			{'name': 'Korean Catholic Church',
+			'address': '849 Don Mills Rd, North York ON M3C 1W1'},
+			{'name': 'Ryerson University',
+			'address': '350 Victoria Street, Toronto Ontario M5B 2K3'},
+			{'name': 'Mattamy Athletic centre',
+			'address': '60 Carlton St, Toronto ON M5B 1J2'},
+			{'name': 'My Crib',
+			'address': '209 Fort York blvd, Toronto ON M5V 4A1'}
+		];
 
-	var R = 6371000; // metres
+		//may have to declare this outside
 
-	var x1 = Math.toRadians(lat1);
+		$scope.getMap = function() {
+			//debug purposes
+			console.log("Starting map fetch");
 
-	var x2 = Math.toRadians(lat2);
-	var y1 = Math.toRadians(lat2-lat1);
-	var y2 = Math.toRadians(lon2-lon1);
+			map = new Microsoft.Maps.Map(document.getElementById("mapDisplay"), 
+			{credentials: "Arhv5WenzBkSQVEgddiB53B9tPQFSz98Ji2lBsMprNS6UeGXGsU_wkC50hrdY9ip",
+			center: new  Microsoft.Maps.Location(43.7000, -79.4000),
+			zoom: 10
+		});
 
-	var a = Math.sin(y1/2) * Math.sin(y1/2) +
-	        Math.cos(x1) * Math.cos(x2) *
-	        Math.sin(y2/2) * Math.sin(y2/2);
-	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+			Microsoft.Maps.loadModule('Microsoft.Maps.Search', {callback: null});
+		}
 
-	var d = R * c;
+		$scope.getListings = function(listings) {
+			for (var i = 0; i < listings.length; i++) {
+				console.log("listing" + i + " : " + listings[i].name);
+			};
+			
+		}
+		
+		$scope.getPinLocation = function(listing){
+			var searchManager = new Microsoft.Maps.Search.SearchManager(map);
+			
+			var geocodeRequest = {where:listing.address, count:10, callback:createMapPin, errorCallback:null};
+			searchManager.geocode(geocodeRequest);
+		}
 
-	//set DOM element to result
-	res.innerHTML = d;
-	console.log("distance is: " + d + " meters");
-}
-	
+		function createMapPin(geocodeResult){
+			console.log("Creating pin for address: " + geocodeResult);
+
+			var pin = new Microsoft.Maps.Pushpin(geocodeResult, null);
+			Microsoft.Maps.Events.addHandler(pin, 'click', function () {
+				search_showInfoBox(result)
+			});
+			map.entities.push(pin);
+			console.log("pin should be showing now");
+		}
+
+		$scope.search_showInfoBox = function(listing){
+			if(currInfobox){
+				currInfobox.setOptions({ visible: true});
+				map.entities.remove(currInfobox);
+			}
+
+			currInfobox = new Microsoft.Maps.Infobox(
+				listing.address,
+				{
+					title: listing.name,
+					description: [listing.address].join(''),
+					showPointer: true,
+					titleAction: null, 
+					titleClickHandler: null
+				});
+			currInfobox.setOptions({visible:true});
+			map.entities.push(currInfobox);
+		}
+	}
+]);														
+
+
+
